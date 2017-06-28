3c2b85591e258c16f4369ee34de55a98a126e9a7
张久奎 <zhangjiukui@gmail.com>
2017-06-28 09:17:15 +0800

    新建空文件

---
+++ CppLearn/CppLearn/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CppLearn
+//
+//  Created by 张久奎 on 2017/6/28.
+//  Copyright © 2017年 张久奎. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
