48a3f5496af3276dd1da28d38ea353627c5e189c
张久奎 <zhangjiukui@gmail.com>
2017-06-28 15:56:43 +0800

    C++for_each用法

--- CppLearn/CppLearn/main.cpp
+++ CppLearn/CppLearn/main.cpp
@@ -7,9 +7,18 @@
 //
 
 #include <iostream>
-
+#include <vector>
+using namespace std;
+void printElem(int& elem) {
+    cout << elem << endl;
+}
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    vector<int> vec;
+    for (int i = 0; i < 5; i++)
+    {
+        vec.push_back(i * i);
+    }
+    for_each(vec.begin(), vec.end(), printElem);
     return 0;
 }
